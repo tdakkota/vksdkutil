7346f0039998fe1e7e3b6655a484e209e1fe5990
tdakkota <tanc13@yandex.ru>
2020-09-14 23:26:18 +0300

    feat: add GetterFunc type

--- middleware/token/storage.go
+++ middleware/token/storage.go
@@ -5,3 +5,9 @@ import "github.com/SevereCloud/vksdk/v2/api"
 type Getter interface {
 	Get(string, ...api.Params) (string, error)
 }
+
+type GetterFunc func(string, ...api.Params) (string, error)
+
+func (g GetterFunc) Get(s string, p ...api.Params) (string, error) {
+	return g(s, p...)
+}
