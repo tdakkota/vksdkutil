591722c847a9ffb5940bd54fb7fb555c4e1b19b7
tdakkota <tanc13@yandex.ru>
2020-09-18 18:37:26 +0300

    docs: add some testcase docs

--- testutil/case.go
+++ testutil/case.go
@@ -8,6 +8,7 @@ import (
 	sdkutil "github.com/tdakkota/vksdkutil/v2"
 )
 
+// TestCase is VK SDK testcase.
 type TestCase struct {
 	Expectations     Expectations
 	T                *testing.T
@@ -15,14 +16,17 @@ type TestCase struct {
 	allowNotExpected bool
 }
 
+// NewTestCase creates new TestCase.
 func NewTestCase(t *testing.T) *TestCase {
 	return &TestCase{Expectations: Expectations{}, T: t}
 }
 
+// AllowNotExpected allows not expected calls.
 func (test *TestCase) AllowNotExpected() {
 	test.allowNotExpected = true
 }
 
+// ExpectCall adds call expectation.
 func (test *TestCase) ExpectCall(method string) *Expectation {
 	p := NewExpectation(method)
 	test.Expectations.Push(p)
@@ -51,8 +55,10 @@ func (test *TestCase) ExpectationsWereMet() error {
 	return nil
 }
 
+// ErrNotExpected is expectation error.
 var ErrNotExpected = fmt.Errorf("call is not expected")
 
+// Handler is mocking VK SDK handler.
 func (test *TestCase) Handler() sdkutil.Handler {
 	return func(method string, params ...api.Params) (api.Response, error) {
 		r, ok := test.Expectations.Pop()
@@ -72,19 +78,18 @@ func (test *TestCase) Handler() sdkutil.Handler {
 	}
 }
 
+// WithSDK calls CreateSDK and passes result to callback.
+// Fails if not ExpectationsWereMet after f parameter call.
 func WithSDK(t *testing.T, f func(*testing.T, *api.VK, *TestCase)) {
 	t.Helper()
 
 	sdk, testCase := CreateSDK(t)
-	defer func() {
-		if err := testCase.ExpectationsWereMet(); err != nil {
-			t.Error(err)
-		}
-	}()
+	defer testCase.ExpectationsWereMet()
 
 	f(t, sdk, testCase)
 }
 
+// CreateSDK creates new testing VK SDK and TestCase.
 func CreateSDK(t *testing.T) (*api.VK, *TestCase) {
 	t.Helper()
 
